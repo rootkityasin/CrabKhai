11e5e812e54a4574a0987e43899d424580fd5ed8
rootkityasin <107543212+rootkityasin@users.noreply.github.com>
2026-01-02 16:14:32 +0600

    Fix auth id type mismatch for build

--- auth.ts
+++ auth.ts
@@ -42,7 +42,12 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
 
                 const passwordsMatch = await bcrypt.compare(credentials.password as string, user.password)
 
-                if (passwordsMatch) return user
+                if (passwordsMatch) {
+                    return {
+                        ...user,
+                        id: user.id.toString(),
+                    }
+                }
                 return null
             }
         }),
