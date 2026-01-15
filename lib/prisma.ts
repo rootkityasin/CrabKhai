1228704fd6a23b05a81f5ddad3ff87ba78f5f38a
rootkityasin <107543212+rootkityasin@users.noreply.github.com>
2026-01-16 00:46:14 +0600

    fix(prisma): configure pg adapter to resolve verified build error

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,9 +1,20 @@
 import { PrismaClient } from '@prisma/client'
+import { Pool } from 'pg'
+import { PrismaPg } from '@prisma/adapter-pg'
 
-const globalForPrisma = globalThis as unknown as { prisma_v2: PrismaClient }
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
 
-export const prisma = globalForPrisma.prisma_v2 || new PrismaClient({
-    log: ['query', 'error', 'warn'],
-})
+const connectionString = `${process.env.DATABASE_URL}`
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma_v2 = prisma
+const pool = new Pool({ connectionString })
+const adapter = new PrismaPg(pool)
+
+export const prisma =
+    globalForPrisma.prisma ||
+    new PrismaClient({
+        adapter,
+        log: ['query', 'error', 'warn'],
+    })
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+
